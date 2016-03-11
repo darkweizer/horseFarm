a26c890b98d7108354fdf0bf7e02f2ff69008faf
KenAdBlock <kennycg13@gmail.com>
2016-03-11 16:47:21 +0100

    Integration Materialize (ligne 23 à décommenter et c'est OK)

--- php/index.php
+++ php/index.php
@@ -10,54 +10,77 @@ and open the template in the editor.
 <html>
 	<head>
 		<title>HorseFarm</title>
-		<link rel="stylesheet" type="text/css" href="../css/estylo.css" media="screen" />
+
+		<!--Import Roboto Font-->
+		<link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'/>
+		<!--Import Google Icon Font-->
+		<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+		<!--Import materialize.css-->
+
+<!--	LIGNE A AJOUTER POUR UTILISER MATERIALIZE
+		LIGNE A AJOUTER POUR UTILISER MATERIALIZE
+		LIGNE A AJOUTER POUR UTILISER MATERIALIZE
+	        <link type="text/css" rel="stylesheet" href="../vendor/materialize/css/materialize.min.css"  media="screen,projection"/>-->
+
+			<!--Import autre(s) .css-->
+		<link type="text/css" rel="stylesheet" href="../css/estylo.css" media="screen" />
+		<link type="text/css" rel="stylesheet" href="../css/style.css"  media="screen,projection"/>
+
+		<!--Let browser know website is optimized for mobile-->
+		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	</head>
 
 	<body text="black" bgcolor="#7F7226" link="black" vlink="black" alink="black">
-		<div id="logo">
-			<img src="../img/logo.png" />
-			<div id="user" class="datos"></div>
-		</div>
-		<header id="header">
-			<div>
-				<nav>
-					<ul>
-						<li><a href="index.php">Accueil</a></li>
-						<li><a href="">Chevaux</a></li>
-						<li><a href="">'Objets'</a></li>
 
-						<li><a href="index.php">À propos</a></li>
-					</ul>
-				</nav>
+			<div id="logo">
+				<img src="../img/logo.png" />
+				<div id="user" class="datos"></div>
 			</div>
-		</header>
+			<header id="header">
+				<div>
+					<nav>
+						<ul>
+							<li><a href="index.php">Accueil</a></li>
+							<li><a href="">Chevaux</a></li>
+							<li><a href="">'Objets'</a></li>
 
-		<div id="titlelistaprov">
-			<h4>Chevaux</h4>
-			<div id="cajabusqueda">
-				<input id="filter" onkeyup="listafiltroprov()" type="text" placeholder="Search..." />
-			</div>
-			<div id="bottonadd">
-				<a href="#modal1"><img src="../img/addprov.png"></a>
-			</div>
-		</div>
+							<li><a href="index.php">À propos</a></li>
+						</ul>
+					</nav>
+				</div>
+			</header>
 
-		<div id="lista"><?php include 'objetos.php';?></div>
-
-		<footer id="foter">This is a test of horseFarm</footer>
-
-		<!-- Formulaire d'ajout (div cachée tant qu'on ne clique pas sur le bouton +(bottonadd) ) -->
-		<div id="modal1" class="modalmask">
-			<div class="modalbox movedown">
-				<div id="titleformreg">
-					<p>Form Edicion</p>
-					<a href="#close" title="Close" class="close" onclick="borrar()">X</a><br>
+			<div id="titlelistaprov">
+				<h4>Chevaux</h4>
+				<div id="cajabusqueda">
+					<input id="filter" onkeyup="listafiltroprov()" type="text" placeholder="Search..." />
+				</div>
+				<div id="bottonadd">
+					<a href="#modal1"><img src="../img/addprov.png"></a>
 				</div>
 			</div>
-		</div>
+
+			<div id="lista"><?php include 'objetos.php';?></div>
+
+			<footer id="foter">This is a test of horseFarm</footer>
+
+			<!-- Formulaire d'ajout (div cachée tant qu'on ne clique pas sur le bouton +(bottonadd) ) -->
+			<div id="modal1" class="modalmask">
+				<div class="modalbox movedown">
+					<div id="titleformreg">
+						<p>Form Edicion</p>
+						<a href="#close" title="Close" class="close" onclick="borrar()">X</a><br>
+					</div>
+				</div>
+			</div>
+
+		<!-- Scripts -->
 
 		<script src="../js/Ajax.js" ></script>
 		<script src="../js/filterHorse.js" ></script>
+		<!-- Framework Materialize -->
+		<script type="text/javascript" src="../vendor/jquery/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="../vendor/materialize/js/materialize.min.js"></script>
 
 	</body >
 </html> 
--- php/view/pageview.php
+++ php/view/pageview.php
@@ -67,7 +67,7 @@ class Pageview {
             <!--Import Google Icon Font-->
             <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
             <!--Import materialize.css-->
-            <link type="text/css" rel="stylesheet" href="../../vendor/css/materialize.min.css"  media="screen,projection"/>
+            <link type="text/css" rel="stylesheet" href="../../vendor/materialize/css/materialize.min.css"  media="screen,projection"/>
             <!--Import style.css-->
             <link type="text/css" rel="stylesheet" href="../../css/style.css"  media="screen,projection"/>
 
@@ -84,8 +84,8 @@ class Pageview {
     public function showJavaLinks() {
         ?>
         <!--Import jQuery before materialize.js-->
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
-        <script type="text/javascript" src="../../vendor/js/materialize.min.js"></script>
+        <script type="text/javascript" src="../../vendor/jquery/jquery-2.1.1.min.js"></script>
+        <script type="text/javascript" src="../../vendor/materialize/js/materialize.min.js"></script>
         <script type="text/javascript" src="../../js/showmessage.js"></script>
         <?php
     }
