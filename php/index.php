0b223fe3b6a47033376d1acc7fa6514feb6e2945
KenAdBlock <kennycg13@gmail.com>
2016-03-11 15:06:41 +0100

    Norme et indentation php/index.php et ajout .htaccess dans le .gitignore pour pb de port different en localhost

--- php/index.php
+++ php/index.php
@@ -8,49 +8,55 @@ and open the template in the editor.
 
 
 <html>
-<head>
-<title>HorseFarm</title>
-<link rel="stylesheet" type="text/css" href="../css/estylo.css" media="screen" />
-</head>
-<body text="black" bgcolor="#7F7226" link="black" vlink="black" alink="black">
-<div id="logo">
-		<img src="../img/logo.png">
-		<div id="user" class="datos"></div>
-</div>
-<header id="header">
-	<div>
-		<nav>
-			<ul>
-				<li><a href="index.php">About us</a></li>
-				<li><a href="">Horses</a></li>
-				<li><a href="prod.php">Products</a></li>
-			
-				<li><a href="Signout.php" >Sign out</a></li>
-			</ul>
-		</nav>
-	</div>
-</header>
-<div id="titlelistaprov">Horses
-<div id="cajabusqueda"><input id="filter" onkeyup="listafiltroprov()" type="text" placeholder="Search..."></div>
-<div id="bottonadd"><a href="#modal1"><img src="../img/addprov.png"></a></div>
-</div>
+	<head>
+		<title>HorseFarm</title>
+		<link rel="stylesheet" type="text/css" href="../css/estylo.css" media="screen" />
+	</head>
+	<body text="black" bgcolor="#7F7226" link="black" vlink="black" alink="black">
+		<div id="logo">
+			<img src="../img/logo.png" />
+			<div id="user" class="datos"></div>
+		</div>
+		<header id="header">
+			<div>
+				<nav>
+					<ul>
+						<li><a href="index.php">Accueil</a></li>
+						<li><a href="">Chevaux</a></li>
+						<li><a href="">'Objets'</a></li>
 
-<div id="lista"><?php include 'objetos.php';?></div>
-<footer id="foter">This is a test of horseFarm</footer>
+						<li><a href="index.php">À propos</a></li>
+					</ul>
+				</nav>
+			</div>
+		</header>
+
+		<div id="titlelistaprov">
+			<h4>Chevaux</h4>
+			<div id="cajabusqueda">
+				<input id="filter" onkeyup="listafiltroprov()" type="text" placeholder="Search..." />
+			</div>
+			<div id="bottonadd">
+				<a href="#modal1"><img src="../img/addprov.png"></a>
+			</div>
+		</div>
+
+		<div id="lista"><?php include 'objetos.php';?></div>
+		<footer id="foter">This is a test of horseFarm</footer>
 
 
+		<div id="modal1" class="modalmask">
+			<div class="modalbox movedown">
+				<div id="titleformreg">
+					<p>Form Edicion</p>
+					<a href="#close" title="Close" class="close" onclick="borrar()">X</a><br>
+				</div>
+			</div>
+		</div>
 
+		<script src="../js/Ajax.js" ></script>
+		<script src="../js/filterHorse.js" ></script>
 
-<div id="modal1" class="modalmask">
-		<div class="modalbox movedown">
-
-			<div id="titleformreg">Form Edicion <a href="#close" title="Close" class="close" onclick="borrar()">X</a><br></div>
-			
-
-	</div>
-	<script src="../js/Ajax.js" ></script>
-	<script src="../js/filterHorse.js" ></script>
-	
-</body >
+	</body >
 </html> 
 
